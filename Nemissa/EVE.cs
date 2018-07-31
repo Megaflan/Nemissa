00c0d1856a6d944c9d20ff869a6edfd86b0d1418
Megaflan <smtivesp@gmail.com>
2018-07-31 06:43:38 +0200

    Refactoring, Yarhl library insertion & early placeholder control code parsing function. Basically, all the program is rewritten.

---
+++ Nemissa/EVE.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nemissa
+{
+    class EVE
+    {
+            class Header
+            {
+                public ushort const1;
+                public ushort const2;
+                public ushort pointerOffset;
+                public ushort textOffset;
+            }
+
+            private Stream fstream;
+
+            private Header header = new Header();
+            private int entryCount;
+            private List<short> pointer;
+
+            System.Text.Encoding SJIS = System.Text.Encoding.GetEncoding(932);
+
+            public EVE(Stream file)
+            {
+                fstream = file;
+                Initialize();
+                ParsePointer();
+                Read();
+            }
+
+            private void Initialize()
+            {
+                using (var br = new BinaryReader(fstream, SJIS, true))
+                {
+                    header.const1 = br.ReadUInt16();
+                    header.const2 = br.ReadUInt16();
+                    header.pointerOffset = br.ReadUInt16();
+                    header.textOffset = br.ReadUInt16();
+                    entryCount = (header.textOffset - header.pointerOffset) / sizeof(short);
+                }
+            }
+
+            private void ParsePointer()
+            {
+                pointer = new List<short>();
+                using (var br = new BinaryReader(fstream, SJIS, true))
+                {
+                    br.BaseStream.Position = header.pointerOffset;
+                    for (int i = 0; i < entryCount; i++)
+                    {
+                        var textP = br.ReadInt16();
+                        if (textP != 0 && i != 0) pointer.Add(br.ReadInt16());
+                    }
+                }
+                entryCount = pointer.Count;
+            }
+
+            private string ParseTextEntry(byte[] toParse)
+            {
+                // TODO
+                // byte[0] == 0xFF, byte[0] == 0x0B, byte[0] == 0x0C and byte[0] == 0xC7
+                // Control codes written in pairs, ex. {FF0B}
+                // toParse will be all the bytes in the entry (Line 77)
+                // Search through the byte array created by toParse and use switch / case to deal with them
+            }
+
+            private string ReadEntry(int n, int size)
+            {
+                using (var br = new BinaryReader(fstream, SJIS, true))
+                {
+                    br.BaseStream.Position = pointer[n] + header.textOffset;
+                    var toParse = br.ReadBytes(size);
+                    return ParseTextEntry(toParse);
+                }
+            }
+
+            public string Read()
+            {
+                string result = "";
+                for (int i = 0; i < entryCount; i++)
+                {
+                    var size = (i + 1 == entryCount) ? fstream.Length - (pointer[i] + header.textOffset) : pointer[i + 1] - pointer[i];
+                    var entry = ReadEntry(i, (int)size);
+                    result += entry + "\n";
+                }
+                return result;
+            }
+    }
+}
--- Nemissa/Program.cs
+++ Nemissa/Program.cs
@@ -12,158 +12,39 @@ namespace Nemissa
     {
         static void Main(string[] args)
         {
-            List<int> offsets = new List<int>();
-            List<int> checkedBytes = new List<int>();
-            List<string> strings = new List<string>();
-            
-            using (BinaryReader br = new BinaryReader(File.Open(args[0], FileMode.Open)))
+            switch (args[0])
             {
-                //Header identification
-                br.BaseStream.Position = 0x4;
-                ushort tablePos = br.ReadUInt16();
-                ushort textPos = br.ReadUInt16();
-
-                //Pointer interpretation
-                br.BaseStream.Position = tablePos;
-                int offlen = textPos - tablePos;
-                for (int i = 0; i < offlen / 2; i++)
-                {
-                    ushort off = br.ReadUInt16();
-                    offsets.Add(off);
-                }
-
-                //End of pointer table
-                br.BaseStream.Position = textPos + offsets.Last();
-                byte checkByte = 0;
-                bool endFound = false;
-                while (endFound != true)
-                {
-                    while (checkByte != 0xFF)
+                case "-po":
+                    switch (Path.GetExtension(args[1]).ToLower())
                     {
-                        checkByte = br.ReadByte();
-                        checkedBytes.Add(checkByte);
-                    }
-                    checkByte = br.ReadByte();
-                    checkedBytes.Add(checkByte);
-                    if (checkByte == 0)
-                    {
-                        endFound = true;
-                    }
-                }
-                int lastPointerEnds = offsets.Last() + checkedBytes.Count();
-
-                //Text interpretation
-                var sjis = Encoding.GetEncoding("sjis");
-                var len = 0;
-                for (int i = 0; i < offsets.Count; i++)
-                {
-                    br.BaseStream.Position = textPos + offsets[i];
-                    if (i != offsets.Count - 1)
-                    {
-                        len = offsets[i + 1] - offsets[i];
-                    }
-                    else
-                    {
-                        len = lastPointerEnds - offsets[i];
-                    }
-                    byte[] str = br.ReadBytes(len);
-                    var convString = sjis.GetString(str);
-                    strings.Add(convString);
-                    Console.WriteLine("\n[Entry " + i + "]");
-                    Console.WriteLine(convString);
-                }
-
-                //Search multiple files
-                Console.WriteLine("--------------------------------");
-                Console.WriteLine("Length of file: " + br.BaseStream.Length);
-                Console.WriteLine("Bytes readen: " + br.BaseStream.Position);
-                if (br.BaseStream.Length > br.BaseStream.Position)
-                {
-                    Console.WriteLine("//New entries found//");
-                    Console.WriteLine("--------------------------------");
-                    br.BaseStream.Position = br.BaseStream.Position + 0x1;
-                    while (br.BaseStream.Length >= br.BaseStream.Position)
-                    {
-                        //Preparation
-                        checkedBytes.Clear();
-                        offsets.Clear();
-
-                        //Header identification
-                        int nextText = (int)br.BaseStream.Position;
-                        br.BaseStream.Position = nextText + 0x4;
-                        if (br.BaseStream.Position >= br.BaseStream.Length)
-                        {
-                            break;
-                        }
-                        tablePos = br.ReadUInt16();
-                        textPos = br.ReadUInt16();
-
-                        //Pointer interpretation
-                        br.BaseStream.Position = nextText + tablePos;
-                        offlen = textPos - tablePos;
-                        for (int i = 0; i < offlen / 2; i++)
-                        {
-                            ushort off = br.ReadUInt16();
-                            if (i != offlen)
+                        case ".eve":
+                            using (BinaryReader br = new BinaryReader(File.OpenRead(args[1])))
                             {
-                                if (off != 0)
+                                File.WriteAllText(args[1] + ".xml", PO.Placeholder);
+                            }
+                            break;
+                        default:
+                            try
+                            {
+                                foreach (var filefound in Directory.GetFiles(args[1], "*.eve", SearchOption.AllDirectories))
                                 {
-                                    offsets.Add(off);
+                                    File.WriteAllText(filefound + ".xml", PO.Placeholder);
                                 }
                             }
-                            else
+                            catch (Exception ex)
                             {
-                                offsets.Add(off);
+                                Console.WriteLine("ERROR: No EVE files found");
+                                Console.WriteLine(ex);
+                                Console.ReadLine();
                             }
-                        }
-
-                        //End of pointer table
-                        br.BaseStream.Position = (nextText + textPos) + offsets.Last();
-                        checkByte = 0;
-                        endFound = false;
-                        int endFoundPos = 0;
-                        while (endFound != true)
-                        {
-                            while (checkByte != 0xFF)
-                            {
-                                checkByte = br.ReadByte();
-                                checkedBytes.Add(checkByte);
-                            }
-                            checkByte = br.ReadByte();
-                            checkedBytes.Add(checkByte);
-                            if (checkByte == 0)
-                            {
-                                endFound = true;
-                                endFoundPos = (int)br.BaseStream.Position;
-                            }
-                        }
-                        lastPointerEnds = nextText + textPos + offsets.Last() + checkedBytes.Count();
-
-                        //Text interpretation
-                        sjis = Encoding.GetEncoding("sjis");
-                        len = 0;
-                        for (int i = 0; i < offsets.Count; i++)
-                        {
-                            br.BaseStream.Position = nextText + textPos + offsets[i];
-                            if (i != offsets.Count - 1)
-                            {
-                                len = offsets[i + 1] - offsets[i];
-                            }
-                            else
-                            {
-                                len = endFoundPos - (nextText + textPos + offsets[i]);
-                            }
-                            byte[] str = br.ReadBytes(len);
-                            var convString = sjis.GetString(str);
-                            strings.Add(convString);
-                            Console.WriteLine("\n[Entry " + i + "]");
-                            Console.WriteLine(convString);
-                        }
+                            //recursive search
+                            break;
                     }
-                }
-                Console.WriteLine("--------------------------------");
-                Console.ReadLine();
+                    break;
+                case "-eve":
+                    break;
             }
         }
+
     }
 }
